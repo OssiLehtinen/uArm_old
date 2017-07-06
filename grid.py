167e94f8da56b72837708e0fafece25f2557b68b
OssiLehtinen <ossi.lehtinen@gmail.com>
2017-07-06 09:22:51 +0300

    Add files via upload

--- grid.py
+++ grid.py
@@ -19,25 +19,31 @@ myRobot.mode(mode)   # Set mode to Normal
 
 
 
+# Larger grid
+#gridSizeX = 120
+#gridSizeY = 200
+#gridOffsetX = 140
 
+# Smaller test grid
+gridSizeX = 40
+gridSizeY = 40
+gridOffsetX = 180
 
-gridSizeX = 150
-gridSizeY = 200
-gridOffsetX = 100
 
 workingHeight = 150
+drawSpeed = 1000
 
 # Horizontal lines 
-for i in range(gridOffsetX/10, (gridOffsetX+gridSizeX)/10+1):
+for i in range(int(gridOffsetX/10), int((gridOffsetX+gridSizeX)/10+1)):
 	print(i*10)
 	myRobot.goto(i*10, -gridSizeY/2, workingHeight, 6000)
-	myRobot.goto_laser(i*10, gridSizeY/2, workingHeight, 1500)
+	myRobot.goto_laser(i*10, gridSizeY/2, workingHeight, drawSpeed)
 	myRobot.goto(i*10, gridSizeY/2, workingHeight, 6000) # Switch the laser off
 
 # Vertical lines
-for i in range(-gridSizeY/20, gridSizeY/20+1):
+for i in range(int(-gridSizeY/20), int(gridSizeY/20+1)):
 	print(i*10)
 	myRobot.goto(gridOffsetX+gridSizeX, i*10, workingHeight, 6000)
-	myRobot.goto_laser(gridOffsetX, i*10, workingHeight, 1500)
+	myRobot.goto_laser(gridOffsetX, i*10, workingHeight, drawSpeed)
 	myRobot.goto(gridOffsetX, i*10, workingHeight, 6000) # Switch the laser off
 
